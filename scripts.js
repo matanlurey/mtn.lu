c04dbc4ff219a80061b67a030f6eb8d481e7d335
matanlurey <matanlurey@users.noreply.github.com>
2024-05-11 19:46:42 +0000

    deploy: 811992cadf975d35debdf788395b6ee997c9687f

---
+++ scripts.js
@@ -0,0 +1,21 @@
+(() => {
+  // If the system prefers dark mode.
+  let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  // If the user has set a preference.
+  if (localStorage.getItem('darkMode') === 'true') {
+    darkMode = true;
+  } else if (localStorage.getItem('darkMode') === 'false') {
+    darkMode = false;
+  }
+
+  // Set the theme.
+  document.body.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+
+  // Toggle the theme.
+  document.querySelector('[data-theme-toggle]').addEventListener('click', () => {
+    darkMode = !darkMode;
+    document.body.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+    localStorage.setItem('darkMode', darkMode);
+  });
+})();
