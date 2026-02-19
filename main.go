11bb5a3f345c2c11bfcf101878ca626ef5455327
Matan Lurey <matan@lurey.org>
2026-02-19 11:57:08 -0800

    Add basic HTTP hello world

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/caarlos0/env/v11"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/caarlos0/env/v11"
+)
 
 // Configures the running application.
 type Config struct {
@@ -17,6 +22,25 @@ func loadConfigFromEnv() Config {
 
 func main() {
 	cfg := loadConfigFromEnv()
-	println("Running on port:", cfg.Port)
-	println("Base URL:", cfg.BaseURL)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprintf(w, `<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<base href="%s">
+	<title>Hello World</title>
+</head>
+<body>
+	<h1>Hello, World!</h1>
+</body>
+</html>`, cfg.BaseURL)
+	})
+
+	addr := fmt.Sprintf(":%d", cfg.Port)
+	fmt.Printf("Listening on %s:%d\n", cfg.BaseURL, cfg.Port)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
 }
